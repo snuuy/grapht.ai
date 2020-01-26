ee249fd44d420565fb44969b52bb2fe9231e0295
bhatts39 <sunnynbhatt@gmail.com>
2020-01-25 21:54:20 -0500

    Login page

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route exact path="/login/:userType" component={LoginPage} />
+        <Route exact path="/login" component={LoginPage} />
       </Switch>
     </BrowserRouter>
   );
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import "../Login.css"
+import logo from '../assets/logo_navy.png'
 export default class LandingPage extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            userType: this.props.match.params.userType
-        }
     }
 
     render() {
         return (
-            <div className="container">
-                LOGIN PAGE for {this.state.userType}
+            <div className="row" style={{ height: "100vh" }}>
+                <div className="col-12 my-auto">
+                    <div className="container h-100">
+                        <div className="row rounded box-shadow bg-white p-5 my-auto">
+                            <div className="col-5 p-4 text-center my-auto py-5">
+                                <img src={logo} className="w-75 my-auto" />
+                            </div>
+                            <div className="col-7 p-4 text-center p-5" style={{ borderLeft: "3px solid #394279" }}>
+                                <div className="px-5 mx-4">
+                                    <div className="color-purple h2 mb-5">Client Login</div>
+                                    <div className="form-group">
+                                        <input type="email" className="form-control" placeholder="E-mail address" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="password" className="form-control" placeholder="Password" />
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <button disabled type="submit" className="rounded btn btn-secondary col-6 float-left">Register</button>
+                                        </div>
+                                        <div className="col-6">
+                                            <a href="/dashboard">
+                                                <button type="submit" className="rounded btn btn-primary col-6 float-right">Login</button>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
