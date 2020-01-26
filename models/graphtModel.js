b515eca4b543344a5562d688becd47be1833f20a
Denesh <denesh100@gmail.com>
2020-01-26 04:51:56 -0500

    adding stuff

--- models/graphtModel.js
+++ models/graphtModel.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 let graphtSchema = new mongoose.Schema({
-    image: String,
-    doctorDiagnosis: {
-        confidence: Number,
-        condition: String
-    },
-    aiDiagnosis: {
-        confidence: Number,
-        condition: String
-    }
+  image: String,
+  doctorDiagnosis: {
+    confidence: Number,
+    condition: String
+  },
+  aiDiagnosis: {
+    confidence: Number,
+    condition: String
+  }
 });
 
 graphtSchema.set("toJSON", { virtuals: true });
